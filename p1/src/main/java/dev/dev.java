ef7c66fe66624df695bc9b5799d7f03171496573
SLekya1 <SLekya1@NEWLSLEKYA10117.Pacific.LathamInt.com>
2017-09-01 17:04:07 -0400

    Added parsed info to map.

--- p1/src/main/java/dev/dev.java
+++ p1/src/main/java/dev/dev.java
@@ -2,13 +2,18 @@ package dev;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.unh.cs.treccar.Data;
 import edu.unh.cs.treccar.read_data.DeserializeData;
 
 public class dev {
+	
 	public static void main(String[] args) throws Exception{
+		
+		Map <String, String> paraMap = new HashMap <String, String>();
+		
         System.setProperty("file.encoding", "UTF-8");
 
         final FileInputStream fileInputStream2 = new FileInputStream(new File("train.test200.cbor.paragraphs"));
@@ -17,6 +22,8 @@ public class dev {
         	String paraId = p.getParaId();
             String text = p.getTextOnly();
             System.out.println("ID " + paraId + "\n" + text + "\n");
+            
+            paraMap.put(paraId, text);
         }
 
         System.out.println("\n\n");
