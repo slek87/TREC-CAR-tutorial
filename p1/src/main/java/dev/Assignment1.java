5598812a1b707bf31ead1e4c64ac48b66eba7d64
SAI <SAI@10.21.42.1>
2017-09-04 16:00:41 -0400

    Reuploaded.

---
+++ p1/src/main/java/dev/Assignment1.java
@@ -0,0 +1,136 @@
+package dev;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BasicStats;
+import org.apache.lucene.search.similarities.SimilarityBase;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import co.nstant.in.cbor.CborException;
+import edu.unh.cs.treccar.Data;
+import edu.unh.cs.treccar.read_data.DeserializeData;
+
+public class Assignment1 {
+
+	static final String INDEX_DIR = "lucene_index/dir";
+	static final String CBOR_FILE = "cbor/train.test200.cbor.paragraphs";
+	
+	private IndexSearcher is = null;
+	private QueryParser qp = null;
+	private boolean customerScore = false;
+
+	public void indexAllParas() throws CborException, IOException {
+		Directory indexdir = FSDirectory.open((new File(INDEX_DIR)).toPath());
+		IndexWriterConfig conf = new IndexWriterConfig(new StandardAnalyzer());
+		IndexWriter iw = new IndexWriter(indexdir, conf);
+		for (Data.Paragraph p : DeserializeData.iterableParagraphs(new FileInputStream(new File(CBOR_FILE)))) {
+			this.indexPara(iw, p);
+		}
+		iw.close();
+	}
+
+	public void indexPara(IndexWriter iw, Data.Paragraph para) throws IOException {
+		Document paradoc = new Document();
+		paradoc.add(new StringField("paraid", para.getParaId(), Field.Store.YES));
+		paradoc.add(new TextField("parabody", para.getTextOnly(), Field.Store.YES));
+		iw.addDocument(paradoc);
+	}
+
+	public void doSearch(String qstring, int n) throws IOException, ParseException {
+		if ( is == null ) {
+			is = new IndexSearcher(DirectoryReader.open(FSDirectory.open((new File(INDEX_DIR).toPath()))));
+		}
+		
+		if ( customerScore ) {
+			SimilarityBase mySimiliarity = new SimilarityBase() {
+				protected float score(BasicStats stats, float freq, float docLen) {
+					return freq;
+				}
+
+				@Override
+				public String toString() {
+					return null;
+				}
+			};
+			is.setSimilarity(mySimiliarity);
+		}
+
+		/*
+		 * The first arg of QueryParser constructor specifies which field of document to
+		 * match with query, here we want to search in the para text, so we chose
+		 * parabody.
+		 * 
+		 */
+		if (qp == null) {
+			qp = new QueryParser("parabody", new StandardAnalyzer());
+		}
+
+		Query q;
+		TopDocs tds;
+		ScoreDoc[] retDocs;
+		
+		System.out.println("Query: " + qstring);
+		q = qp.parse(qstring);
+		tds = is.search(q, n);
+		retDocs = tds.scoreDocs;
+		Document d;
+		for (int i = 0; i < retDocs.length; i++) {
+			d = is.doc(retDocs[i].doc);
+			System.out.println("Doc " + i);
+			System.out.println("Score " + tds.scoreDocs[i].score);
+			System.out.println(d.getField("paraid").stringValue());
+			System.out.println(d.getField("parabody").stringValue() + "\n");
+			
+		}
+	}
+
+	public void customerScore(boolean custom) throws IOException {
+		customerScore = custom;
+	}
+
+	public static void main(String[] args) {
+		Assignment1 a = new Assignment1();
+		int topSearch = 10;
+		String[] queryArr = {"power nap benefits", "whale vocalization production of sound", "pokemon puzzle league"};
+		
+		try {
+			a.indexAllParas();
+			for(String qstring:queryArr) {
+				a.doSearch(qstring, topSearch);
+			}
+			
+			System.out.println(StringUtils.repeat("=", 300));
+			
+			a.customerScore(true);
+			for(String qstring:queryArr) {
+				a.doSearch(qstring, topSearch);
+			}
+			
+		} catch (CborException | IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
