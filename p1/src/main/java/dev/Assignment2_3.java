8772ecb33d77f12eb08c57152c13be8f62139a56
nihilistsumo <kashyapisumanta@gmail.com>
2017-09-18 17:17:05 -0400

    second commit rprec func

---
+++ p1/src/main/java/dev/Assignment2_3.java
@@ -0,0 +1,15 @@
+package dev;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.unh.cs.treccar.Data;
+
+public class Assignment2_3 {
+public HashMap<String, Double> getPageRprecMap(ArrayList<Data.Page> pageList, String qrelsPath, String runPath){
+		HashMap<String, Double> rprecScores = new HashMap<String, Double>();
+		
+		return rprecScores;
+	}
+
+}
